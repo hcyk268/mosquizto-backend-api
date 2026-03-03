c452f0821628f9641bd982449b8e0e6a3aa6711b
hcyk268 <vvhipt206.contact@gmail.com>
2026-03-03 18:51:51 +0700

    Initial commit: Mosquizto API Spring Boot project

---
+++ src/main/java/com/mosquizto/api/MosquiztoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mosquizto.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MosquiztoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MosquiztoBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mosquizto/api/MosquiztoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mosquizto.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MosquiztoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
